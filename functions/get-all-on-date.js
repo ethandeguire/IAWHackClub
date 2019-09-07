d2bd6e50dbfe48bcec6b9d4cf1610aa8aa91139d
Ethan <ethandeguire@gmail.com>
2019-09-07 01:45:51 -0400

    update

--- functions/get-all-on-date.js
+++ functions/get-all-on-date.js
@@ -17,7 +17,13 @@ exports.handler = (event, context, callback) => {
       let objectsOnDate = []
 
       urlObjects.forEach(urlObject => {
-        const date = new Date(new Date(urlObject.ts / 1000).toUTCString())
+        const date = new Date(urlObject.ts / 1000)
+        const date2 = new Date(urlObject['data']['datetime'])
+        console.log(searchDate.getDate(), date.getDate(), date2.getDate())
+        console.log(searchDate, date, date2)
+        console.log('\n\n')
+
+        // console.log(searchDate, formData['date'])
         if (sameDay(date, searchDate)) { objectsOnDate.push(urlObject) }
       });
 
